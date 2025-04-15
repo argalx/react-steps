8145636321f59ad084750a7381a4949ae2da830e
Arvin Bonggal <arvinbonggal@gmail.com>
2025-04-15 16:58:39 +0800

    Added some comments

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ export default function App() {
       {isOpen && (
         <div className="steps">
           <div className="numbers">
+            {/* // Assignment: Make this dynamic */}
             <div className={step >= 1 ? "active" : ""}>1</div>
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
